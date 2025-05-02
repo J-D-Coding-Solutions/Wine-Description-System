ce103dc1a37c12cb8aa1d8c715a6dc6121a0b4a3
Dennis Kramer <denkramer24@gmail.com>
2025-05-01 21:56:49 -0400

    Fav wines working expect for Friend page whooo

--- src/main/java/com/example/springboottutorial/Repository/WineRepository.java
+++ src/main/java/com/example/springboottutorial/Repository/WineRepository.java
@@ -4,7 +4,10 @@ import com.example.springboottutorial.Model.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface WineRepository extends JpaRepository<wines, Long> {
+    Optional<wines> findByWineName(String winename);
 
 }
