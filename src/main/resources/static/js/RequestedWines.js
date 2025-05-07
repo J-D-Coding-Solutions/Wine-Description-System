e0312a7eb0740a7832ac5886f02d6226b092b3f6
Dennis Kramer <denkramer24@gmail.com>
2025-05-07 19:32:48 -0400

    Unit Testing Added

--- src/main/resources/static/js/RequestedWines.js
+++ src/main/resources/static/js/RequestedWines.js
@@ -2,11 +2,11 @@ function searchwine(){
     var input, filter, table, tr, td, i, txtValue;
     input = document.getElementById("searchwine");
     filter = input.value.toUpperCase();
-    table = document.getElementById("wineList");
+    table = document.getElementById("wineTable");
     tr = table.getElementsByTagName("tr");
 
     for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[1];
+        td = tr[i].getElementsByTagName("td")[0];
         if (td) {
             txtValue = td.textContent || td.innerText;
             if (txtValue.toUpperCase().indexOf(filter) > -1) {
@@ -32,34 +32,4 @@ function showwineInfo(thisObj, i){
     textbox.textContent = userId;
 
     infroRow.innerHTML = "<td>" + userId + "</td>" + "<td>" + userName + "</td>";
-}
-
-function addwine(thisObj){
-    var allChildNodes = thisObj.parentNode.parentNode.childNodes;
-    var userId = allChildNodes[1].textContent;
-    var userName = allChildNodes[3].textContent;
-    var friendTable = document.getElementById("friendTable");
-    var row = friendTable.insertRow(-1);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-
-    cell1.innerHTML = userId;
-    cell2.innerHTML = userName;
-
-
-    row.addEventListener('click', function() {
-        showwineInfo(row, 0);
-    });
-
-    var parentRow =  thisObj.parentNode.parentNode;
-    parentRow.remove();
-
-}
-
-function declinewine(thisObj){
-    var parentRow = thisObj.parentNode.parentNode;
-    var confrimDelete = confirm("Are you sure you want to decline this wine request?");
-    if (confrimDelete){
-        parentRow.remove();
-    }
 }
